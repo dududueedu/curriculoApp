016fc1456d4b19cf1ecf349713cd3ef1f920317d
Eduardo <eduardooffside@gmail.com>
2021-06-23 22:29:02 -0300

    add perfil and info

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import pngFoto from './assets/img/perfil.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.containerPerfil}>
+        <Image style={styles.fotoPNG} source={pngFoto} />
+        <Text style={styles.name}>Eduardo da Silva Gomes</Text>
+        <Text style={styles.profissao}>Desenvolvedor Web e Mobile</Text>
+          <View>
+            <Text>GitHub</Text>
+            <Text>Instagram</Text>
+            <Text>LinkedIn</Text>
+          </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +23,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
+  },
+  containerPerfil: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fotoPNG: {
+    height: 140,
+    width: 140
+  }, 
+  name: {
+    fontSize: 26, 
+    fontWeight: 'bold', 
+    marginTop: 10
+  }, 
+  profissao: {
+    color: '#939393', 
+    marginBottom: 7
+  }
 });
