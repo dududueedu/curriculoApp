5a7c72c3578405ef0970d3f2372c34353731fa7d
Eduardo <eduardooffside@gmail.com>
2021-06-25 00:06:24 -0300

    using @expo/vector-icons

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 import pngFoto from './assets/img/perfil.png'
 
 export default function App() {
@@ -10,11 +12,11 @@ export default function App() {
         <Image style={styles.fotoPNG} source={pngFoto} />
         <Text style={styles.name}>Eduardo da Silva Gomes</Text>
         <Text style={styles.profissao}>Desenvolvedor Web e Mobile</Text>
-          <View>
-            <Text>GitHub</Text>
-            <Text>Instagram</Text>
-            <Text>LinkedIn</Text>
-          </View>
+        <View style={styles.redeSociais}>
+          <AntDesign name="github" size={21} color="black" />
+          <FontAwesome5 name="facebook" size={21} color="black" />
+          <Entypo name="linkedin-with-circle" size={21} color="black" />
+        </View>
       </View>
     </View>
   );
@@ -33,14 +35,20 @@ const styles = StyleSheet.create({
   fotoPNG: {
     height: 140,
     width: 140
-  }, 
+  },
   name: {
-    fontSize: 26, 
-    fontWeight: 'bold', 
+    fontSize: 26,
+    fontWeight: 'bold',
     marginTop: 10
-  }, 
+  },
   profissao: {
-    color: '#939393', 
+    color: '#939393',
     marginBottom: 7
+  }, 
+  redeSociais: {
+    flexDirection: 'row',
+    width: '50%',
+    justifyContent: 'space-evenly', 
+    marginTop: 10 
   }
 });
