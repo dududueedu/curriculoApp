b44d8e6d190e7838c6426bafead92925a2e0fbda
Eduardo <eduardooffside@gmail.com>
2021-06-25 17:43:04 -0300

    create handleRedeSocial manage links

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TouchableOpacity, Linking } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
-import pngFoto from './assets/img/perfil.png'
 
+import pngFoto from './assets/img/perfil.png'
 import myStylo from './css/style'
 
 export default function App() {
+
+  function handleRedeSocial(rs) {
+    switch (rs) {
+      case 'github':
+        Linking.openURL('https://github.com/dududueedu')
+        break
+      case 'facebook':
+        Linking.openURL('https://www.facebook.com/profile.php?id=100005503066960')
+        break
+      case 'linkedin':
+        Linking.openURL('https://www.linkedin.com/in/dududueedu/')
+        break
+    }
+  }
+
   return (
     <View style={myStylo.container}>
       <View style={myStylo.containerPerfil}>
@@ -15,9 +30,12 @@ export default function App() {
         <Text style={myStylo.name}>Eduardo da Silva Gomes</Text>
         <Text style={myStylo.profissao}>Desenvolvedor Web e Mobile</Text>
         <View style={myStylo.redeSociais}>
-          <AntDesign name="github" size={21} color="black" />
-          <FontAwesome5 name="facebook" size={21} color="black" />
-          <Entypo name="linkedin-with-circle" size={21} color="black" />
+          <TouchableOpacity onPress={() => handleRedeSocial('github')}>
+            <AntDesign name="github" size={21} color="black" /></TouchableOpacity>
+          <TouchableOpacity onPress={() => handleRedeSocial('facebook')}>
+            <FontAwesome5 name="facebook" size={21} color="black" /></TouchableOpacity>
+          <TouchableOpacity onPress={() => handleRedeSocial('linkedin')}>
+            <Entypo name="linkedin-with-circle" size={21} color="black" /></TouchableOpacity>
         </View>
       </View>
 
@@ -49,7 +67,7 @@ export default function App() {
               Ensino Médio e Curso Técnico de Informática, EEEP Flávio Gomes
               Granjeiro, Paraipaba-CE (2015 – 2017).</Text>
             <Text style={myStylo.cardText}>
-            Bacharelado em Sistemas de Informação, UFC, Quixadá-CE (2018 – atual).</Text>
+              Bacharelado em Sistemas de Informação, UFC, Quixadá-CE (2018 – atual).</Text>
           </View>
         </View>
       </View>
